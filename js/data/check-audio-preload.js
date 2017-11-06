e948178b67807bd94f6318ca3c1162d49d54b93d
Alex <alex.get91@gmail.com>
2017-11-06 12:33:25 +0300

    Добавлена полная предзагрузка аудио перед игрой

---
+++ js/data/check-audio-preload.js
@@ -0,0 +1,12 @@
+import Application from '../application';
+
+let audioPreloadCount = 0;
+
+export default (gameMusicLength) => {
+  audioPreloadCount++;
+
+  console.log(`Загружено ${audioPreloadCount} из ${gameMusicLength}`);
+  if (gameMusicLength === audioPreloadCount) {
+    Application.initialize();
+  }
+};
